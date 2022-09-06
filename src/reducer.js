86fb71e3fe636ae59f02984b8f4b4d557d5c225d
Christopher Callender <christophercallender0000@gmail.com>
2022-09-06 11:15:11 -0400

    Completed assignment

--- src/reducer.js
+++ src/reducer.js
@@ -1,13 +1,23 @@
+let state = { count: 0 };
+
 function changeState(state, action) {
   switch (action.type) {
-    case "counter/increment":
+    case 'counter/increment':
       return { count: state.count + 1 };
     default:
       return state;
   }
 }
 
-let state = { count: 0 };
-let action = { type: "counter/increment" };
+function render() {
+  document.body.textContent = state.count;
+}
 
-changeState(state, action);
+function dispatch(action) {
+  state = changeState(state, action);
+  render();
+}
+
+dispatch({ type: 'counter/increment' }); // {count: 1}
+dispatch({ type: 'counter/increment' }); // {count: 2}
+dispatch({ type: 'counter/increment' }); // {count: 3}
